5321e41c032a669d6419f7036bdc68ee296fd8d5
reed665 <reed665@hotmail.com>
2022-11-11 20:51:09 +0300

    add WithScaleLoadingSpinner

---
+++ src/modules/common/components/WithScaleLoadingSpinner.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+defineProps({
+  loading: Boolean,
+})
+</script>
+
+<template>
+  <div class="with-scale-loading-spinner">
+    <div
+      class="content"
+      :class="{ 'content-loading': loading }"
+    >
+      <slot />
+    </div>
+
+    <scale-loading-spinner
+      v-show="loading"
+      v-bind="$attrs"
+    ></scale-loading-spinner>
+  </div>
+</template>
+
+<style scoped>
+.with-scale-loading-spinner {
+  position: relative;
+}
+
+.content-loading {
+  pointer-events: none;
+  opacity: .66;
+}
+
+scale-loading-spinner {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
--- src/modules/common/components/index.ts
+++ src/modules/common/components/index.ts
@@ -1,3 +1,4 @@
 export { default as AView } from './AView.vue'
 export { default as TheLayout } from './TheLayout.vue'
 export { default as TheNav } from './TheNav.vue'
+export { default as WithScaleLoadingSpinner } from './WithScaleLoadingSpinner.vue'
