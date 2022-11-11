4ca042d93703e5890d5b8459c21644e09eaa07ac
reed665 <reed665@hotmail.com>
2022-11-11 20:51:31 +0300

    add loading state to form

--- src/modules/scale-form-with-vuelidate/views/ScaleFormWithVuelidateView.vue
+++ src/modules/scale-form-with-vuelidate/views/ScaleFormWithVuelidateView.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 import { useVuelidate } from '@vuelidate/core'
 import { required } from '@vuelidate/validators'
 import FormControl from '../components/FormControl.vue'
 import BetterScaleTextField from '../components/BetterScaleTextField.vue'
-import { AView } from '@/modules/common/components'
+import { AView, WithScaleLoadingSpinner } from '@/modules/common/components'
 
 const state = reactive({
   firstName: '',
@@ -16,17 +16,40 @@ const rules = {
   lastName: { required },
 }
 
-const v$ = useVuelidate(rules, state)
+const loadingForm = ref(false)
 
-const onFormSubmit = async () => {
-  await v$.value.$validate()
+const loadForm = (timeout = 1000): Promise<void> => {
+  loadingForm.value = true
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      loadingForm.value = false
+      resolve()
+    }, timeout)
+  })
 }
+
+const v$ = useVuelidate(rules, state)
 
 const resetForm = () => {
   v$.value.$reset()
 
   state.firstName = ''
   state.lastName = ''
+}
+
+const onFormSubmit = async () => {
+  const formIsValid = await v$.value.$validate()
+  if (!formIsValid)
+    return
+
+  try {
+    await loadForm()
+    resetForm()
+  }
+  catch {
+    // do nothing
+  }
 }
 </script>
 
@@ -37,36 +60,41 @@ const resetForm = () => {
     </template>
 
     <form @submit.prevent="onFormSubmit">
-      <FormControl>
-        <BetterScaleTextField
-          v-model="state.firstName"
-          label="First name"
-          :invalid="v$.firstName.$error"
-          :helper-text="v$.firstName.$error ? v$.firstName.$errors[0].$message : ''"
-        />
-      </FormControl>
-
-      <FormControl>
-        <BetterScaleTextField
-          v-model="state.lastName"
-          label="Last name"
-          :invalid="v$.lastName.$error"
-          :helper-text="v$.lastName.$error ? v$.lastName.$errors[0].$message : ''"
-        />
-      </FormControl>
-
-      <scale-button type="submit">
-        Submit
-      </scale-button>
-
-      <scale-button
-        type="button"
-        variant="secondary"
-        style="margin-left: var(--scl-spacing-16);"
-        @click="resetForm"
+      <WithScaleLoadingSpinner
+        size="large"
+        :loading="loadingForm"
       >
-        Reset
-      </scale-button>
+        <FormControl>
+          <BetterScaleTextField
+            v-model="state.firstName"
+            label="First name"
+            :invalid="v$.firstName.$error"
+            :helper-text="v$.firstName.$error ? v$.firstName.$errors[0].$message : ''"
+          />
+        </FormControl>
+
+        <FormControl>
+          <BetterScaleTextField
+            v-model="state.lastName"
+            label="Last name"
+            :invalid="v$.lastName.$error"
+            :helper-text="v$.lastName.$error ? v$.lastName.$errors[0].$message : ''"
+          />
+        </FormControl>
+
+        <scale-button type="submit">
+          Submit
+        </scale-button>
+
+        <scale-button
+          type="button"
+          variant="secondary"
+          style="margin-left: var(--scl-spacing-16);"
+          @click="resetForm"
+        >
+          Reset
+        </scale-button>
+      </WithScaleLoadingSpinner>
     </form>
   </AView>
 </template>
