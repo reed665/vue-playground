089404863b6208904521a2279e9b2adcd8c0a648
reed665 <reed665@hotmail.com>
2022-11-17 09:58:12 +0300

    add vueuse and use UseDark to toggle app theme

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { RouterView } from 'vue-router'
+import { UseDark } from '@vueuse/components'
 import { TheNav } from './modules/common/components'
-import { useEventListener } from './modules/common/composables'
 import { useMousePosition } from './modules/composables-and-renderless-components'
 
 const { listenMouseMove } = useMousePosition()
@@ -10,42 +10,33 @@ const { listenMouseMove } = useMousePosition()
 listenMouseMove()
 
 const drawerOpened = ref(true)
-
-const theme = ref(window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
-
-useEventListener(window.matchMedia('(prefers-color-scheme: dark)'), 'change', (event) => {
-  const newThemeValue = event.matches ? 'dark' : 'light'
-
-  theme.value = newThemeValue
-})
-
-// const toggleTheme = () => {
-//   const newThemeValue = (theme.value === 'light' ? 'dark' : 'light')
-
-//   theme.value = newThemeValue
-// }
 </script>
 
 <template>
-  <v-app :theme="theme">
-    <v-navigation-drawer v-model="drawerOpened">
-      <TheNav />
-    </v-navigation-drawer>
+  <UseDark v-slot="{ isDark, toggleDark }">
+    <v-app
+      :theme="isDark ? 'dark' : 'light'"
+      :data-mode="isDark ? 'dark' : 'light'"
+    >
+      <v-navigation-drawer v-model="drawerOpened">
+        <TheNav />
+      </v-navigation-drawer>
 
-    <v-app-bar>
-      <v-app-bar-nav-icon @click="drawerOpened = !drawerOpened"></v-app-bar-nav-icon>
-      <v-toolbar-title>Vue playground</v-toolbar-title>
+      <v-app-bar>
+        <v-app-bar-nav-icon @click="drawerOpened = !drawerOpened"></v-app-bar-nav-icon>
+        <v-toolbar-title>Vue playground</v-toolbar-title>
 
-      <!-- <v-btn
-        :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-        @click="toggleTheme"
-      >
-        Toggle Theme
-      </v-btn> -->
-    </v-app-bar>
+        <v-btn
+          :prepend-icon="isDark ? 'mdi-weather-night' : 'mdi-weather-sunny'"
+          @click="toggleDark()"
+        >
+          Toggle Theme
+        </v-btn>
+      </v-app-bar>
 
-    <v-main>
-      <RouterView />
-    </v-main>
-  </v-app>
+      <v-main>
+        <RouterView />
+      </v-main>
+    </v-app>
+  </UseDark>
 </template>
