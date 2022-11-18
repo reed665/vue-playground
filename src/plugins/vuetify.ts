f79158907b2367c1de2227cd4ffc172dde800129
reed665 <reed665@hotmail.com>
2022-11-18 11:20:32 +0300

    use vuetify m3 blueprint

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,10 +1,12 @@
 import '../assets/main.scss'
 import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
+import { md3 } from 'vuetify/blueprints'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 export const vuetify = createVuetify({
+  blueprint: md3,
   components,
   directives,
 })
